cfe0e31f2644e78d9b855dbadf945d17f6770617
sagar <sagara@clemson.edu>
2020-12-20 15:24:59 -0500

    Add bug fixedversion

--- rk4/push-rk4.c
+++ rk4/push-rk4.c
@@ -8,13 +8,10 @@
 #include <math.h>
 #define num_steps_per_orbit 100
 
-static const double tfinal                  =  4.0 * M_PI;
+static const double tfinal                  =  80.0 * M_PI;
 static const double charge_of_particle      = -1.0;
 static const double mass_of_particle        =  1.0;
-static const double particle_lorentz_factor =  2.0;
-static const double electric_field[4] = {0.0, 0.0, 0.0, 0.0};
-static const double magnetic_field[4] = {0.0, 0.0, 0.0, 1.0};
-
+static const double particle_lorentz_factor =  3.0;
 
 
 
@@ -31,6 +28,27 @@ double dot(const double a[4], const double b[4])
     return a[1] * b[1] + a[2] * b[2] + a[3] * b[3];
 }
 
+
+
+
+// ============================================================================
+void electric_and_magnetic_fields(const double x[4], double E[4], double B[4])
+{
+    E[0] = 0.0;
+    E[1] = 0.0;
+    E[2] = 0.3;
+    E[3] = 0.0;
+
+    B[0] = 0.0;
+    B[1] = 0.0;
+    B[2] = 0.0;
+    B[3] = 1.0;
+}
+
+
+
+
+// ============================================================================
 double lorentz_factor(const double u[4]) // JJZ: ADDED THIS FUNCTION: u[0] SHOULD NEVER STORE ANYTHING,
                                          // LORENTZ FACTOR SHOULD BE DERIVED INSTEAD. AVOIDS THE NEED TO
                                          // SYNCHRONIZE LOGICALLY REDUNDANT DATA, A MAJOR SOURCE OF BUGS.
@@ -38,24 +56,28 @@ double lorentz_factor(const double u[4]) // JJZ: ADDED THIS FUNCTION: u[0] SHOUL
     return sqrt(1.0 + dot(u, u));
 }
 
-void time_derivative_u(const double E[4], const double B[4], const double x[4], const double u[4], double dudt[4])
+void time_derivative_u(const double x[4], const double u[4], double dudt[4])
 {
     double e = charge_of_particle;
     double m = mass_of_particle;
 
-
     double q_to_m = e / m;
     double u_cross_B[4];
 
+    double E[4], B[4];
+    electric_and_magnetic_fields(x, E, B);
+
     cross(u, B, u_cross_B);
+
+    dudt[0] = 0;
 
     for (int d = 1; d <= 3; ++d)
     {
-        dudt[d] = q_to_m * (E[d] + u_cross_B[d]) / lorentz_factor(u);
+        dudt[d] = q_to_m * E[d] + q_to_m * u_cross_B[d] / lorentz_factor(u);
     }
 }
 
-void time_derivative_x(const double E[4], const double B[4], const double x[4], const double u[4], double dxdt[4])
+void time_derivative_x(const double x[4], const double u[4], double dxdt[4])
 {
     dxdt[0] = 1.0; // JJZ: dt/dt = 1.0
 
@@ -65,7 +87,7 @@ void time_derivative_x(const double E[4], const double B[4], const double x[4], 
     }
 }
 
-void push_particle(const double E[4], const double B[4], double x0[4], double u0[4], const double dt)
+void push_particle(double x0[4], double u0[4], const double dt)
 {
     double x1[4], x2[4], x3[4];
     double u1[4], u2[4], u3[4];
@@ -73,10 +95,9 @@ void push_particle(const double E[4], const double B[4], double x0[4], double u0
     double dudt0[4], dudt1[4], dudt2[4], dudt3[4];
 
 
-    // STEP 1
     // ----------------------------------
-    time_derivative_x(E, B, x0, u0, dxdt0);
-    time_derivative_u(E, B, x0, u0, dudt0);
+    time_derivative_x(x0, u0, dxdt0);
+    time_derivative_u(x0, u0, dudt0);
 
     for (int d = 0; d <= 3; ++d) // JJZ: NOTE THE LOOP STARTS AT 0: NECESSARY TO UPDATE TIME
     {
@@ -84,11 +105,9 @@ void push_particle(const double E[4], const double B[4], double x0[4], double u0
         u1[d] = u0[d] + dudt0[d] * dt * 0.5;
     }
 
-
-    // STEP 2
     // ----------------------------------
-    time_derivative_x(E, B, x1, u1, dxdt1);
-    time_derivative_u(E, B, x1, u1, dudt1);
+    time_derivative_x(x1, u1, dxdt1);
+    time_derivative_u(x1, u1, dudt1);
 
     for (int d = 0; d <= 3; ++d)
     {
@@ -96,11 +115,9 @@ void push_particle(const double E[4], const double B[4], double x0[4], double u0
         u2[d] = u0[d] + dudt1[d] * dt * 0.5;
     }
 
-
-    // STEP 3
     // ----------------------------------
-    time_derivative_x(E, B, x2, u2, dxdt2);
-    time_derivative_u(E, B, x2, u2, dudt2);
+    time_derivative_x(x2, u2, dxdt2);
+    time_derivative_u(x2, u2, dudt2);
 
     for (int d = 0; d <= 3; ++d)
     {
@@ -108,19 +125,17 @@ void push_particle(const double E[4], const double B[4], double x0[4], double u0
         u3[d] = u0[d] + dudt2[d] * dt * 1.0;
     }
 
-
-    // STEP 4
     // ----------------------------------
-    time_derivative_x(E, B, x3, u3, dxdt3);
-    time_derivative_u(E, B, x3, u3, dudt3);
+    time_derivative_x(x3, u3, dxdt3);
+    time_derivative_u(x3, u3, dudt3);
 
 
     // UPDATE POSITION AND GAMMA-BETA
     // ----------------------------------
     for (int d = 0; d <= 3; ++d)
     {
-        x0[d] += (dxdt0[d] + dxdt1[d] * 2.0 + dxdt2[d] * 2.0 + dxdt3[d]) * dt / 6.0;
-        u0[d] += (dudt0[d] + dudt1[d] * 2.0 + dudt2[d] * 2.0 + dudt3[d]) * dt / 6.0;
+        x0[d] += (dxdt0[d] + 2.0 * dxdt1[d] + 2.0 * dxdt2[d] + dxdt3[d]) * dt / 6.0;
+        u0[d] += (dudt0[d] + 2.0 * dudt1[d] + 2.0 * dudt2[d] + dudt3[d]) * dt / 6.0;
     }
 }
 
@@ -131,7 +146,7 @@ void push_particle(const double E[4], const double B[4], double x0[4], double u0
 struct Particle
 {
     double x[4]; // particle position
-    double u[4]; // particle four-velocity  JJZ -- PARTICLE STORES ITS GAMMA-BETA: CLEARER IN MY OPINION
+    double u[4]; // particle four-velocity
 };
 
 
@@ -140,20 +155,18 @@ struct Particle
 // ============================================================================
 double larmor_frequency()
 {
+    struct Particle state;
+    double E[4], B[4];
+    electric_and_magnetic_fields(state.x, E, B);
     double e = charge_of_particle;
-    double b = sqrt(dot(magnetic_field, magnetic_field));
+    double b = sqrt(dot(B, B));
     double m = mass_of_particle;
     double gamma = particle_lorentz_factor;
-    //
-    // JJZ -- CRITICAL BUG BELOW: ORDER OF OPERATIONS. THIS WAS PRESENT ALREADY
-    // IN THE SNIPPET I SENT ON 11-12.
-    //
-    // return fabs(e * b / gamma * m);
 
     return fabs(e * b / gamma / m);
 }
 
-double particle_beta() // JJZ -- RENAMED THIS VELOCITY -> BETA
+double particle_beta()
 {
     double gamma = particle_lorentz_factor;
     return sqrt(1.0 - 1.0 / gamma / gamma);
@@ -168,7 +181,6 @@ double larmor_period()
 {
     return 2 * M_PI / larmor_frequency();
 }
-
 
 
 
@@ -209,7 +221,7 @@ struct Particle advance(struct Particle state)
 {
     double dt = larmor_period() / num_steps_per_orbit;
 
-    push_particle(electric_field, magnetic_field, state.x, state.u, dt);
+    push_particle(state.x, state.u, dt);
 
     return state;
 }
@@ -221,22 +233,20 @@ struct Particle advance(struct Particle state)
 int main()
 {
     struct Particle state = initial_state();
-    double e_exact = mass_of_particle * dot(state.u, state.u) / particle_lorentz_factor;
-    
+    double e_exact = mass_of_particle * (particle_lorentz_factor - 1.0);
+    double x_exact[4];
 
     FILE* outfile = fopen("updated-solution.dat", "w");
 
     while (state.x[0] < tfinal)
     {
         state = advance(state);
-        double vel2 = dot(state.u, state.u);
-        double energy = mass_of_particle * vel2 / lorentz_factor(state.u);
-        double x_exact[4];
+        double energy = mass_of_particle * (lorentz_factor(state.u) - 1.0);
         exact_particle_position(state.x[0], x_exact);
-        double exact_phase = atan2(x_exact[2], x_exact[1]);
-        double phase       = atan2(state.x[2], state.x[1]);
+        //double exact_phase = atan2(x_exact[2], x_exact[1]);
+        //double phase       = atan2(state.x[2], state.x[1]);
         //fprintf(outfile, "%+.09e %+.09e %+.09e\n", state.x[0], exact_phase, phase);
-        fprintf(outfile, "%+.09e %+.09e %+.09e\n", state.x[0], x_exact[1], state.x[1]);
+        fprintf(outfile, "%+.09e %+.09e %+.09e %+.09e %+.09e\n", state.x[0], x_exact[1], x_exact[2], state.x[1], state.x[2]);
     }
     fclose(outfile);
     return 0;
